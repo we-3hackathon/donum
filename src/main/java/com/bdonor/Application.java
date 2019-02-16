73986fc6c0e16b8b3f7a1f4ebe65da86c3fbe8cb
Sharjeel50 <sharjeel50@hotmail.co.uk>
2019-02-16 18:07:48 +0000

    First Commit

--- src/main/java/com/bdonor/Application.java
+++ src/main/java/com/bdonor/Application.java
@@ -1,15 +1,23 @@
-package com.
-
+package com.bdonor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class Application {
 
 	public static void main(String[] args) {
+		System.out.println("hello world");
 		SpringApplication.run(Application.class, args);
-		
+	}
 
+	@RequestMapping("Home")
+	@ResponseBody
+	public String getString(){
+		return "Home does this work";
 	}
 
 }
