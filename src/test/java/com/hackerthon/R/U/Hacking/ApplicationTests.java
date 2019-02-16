32143d646cb812c1cc11ec1e98f7a291816cc4b3
Sumite Ramchande <aiden@Sumites-MacBook-Pro.local>
2019-02-16 20:27:26 +0000

    pom.xml

--- src/test/java/com/hackerthon/R/U/Hacking/ApplicationTests.java
+++ src/test/java/com/hackerthon/R/U/Hacking/ApplicationTests.java
@@ -1,7 +1,5 @@
 package com.hackerthon.R.U.Hacking;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
