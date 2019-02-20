a0edf95ff0245c2c445309f19bcb80901f901ff9
Daniel Thavapalan <Daniel.Thavapalan@gmail.com>
2019-02-20 22:05:24 +0000

    Added Index(Main) View
    I have added the structure of the index view which we need to elaborate on.

--- src/main/java/com/bdonor/springmain/controller/MainController.java
+++ src/main/java/com/bdonor/springmain/controller/MainController.java
@@ -5,10 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-public class UserController {
-    @RequestMapping(value = "/")
-    public String register(){
-        return "Register.html";
-    }
-
+public class MainController {
+    @RequestMapping(value = "/index")
+    public String Index(){ return "index.html"; }
 }
+
+
+
+
