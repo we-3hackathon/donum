c237f9f8fa5de54b2f2cd5b160e6155a9d9c94fe
Sharjeel Jan <41446219+Sharjeel50@users.noreply.github.com>
2019-03-19 21:12:40 +0000

    Service a (#12)
    * Account Service Complete

--- as-service/src/main/java/com/AccountService/accountService/AccountServiceApplication.java
+++ as-service/src/main/java/com/AccountService/accountService/AccountServiceApplication.java
@@ -1,7 +1,8 @@
-package com.bdonor.accountservice;
+package com.AccountService.accountService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class AccountServiceApplication {
--- as-service/src/test/java/com/AccountService/accountService/AccountServiceApplicationTests.java
+++ as-service/src/test/java/com/AccountService/accountService/AccountServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bdonor.accountservice;
+package com.AccountService.accountService;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
