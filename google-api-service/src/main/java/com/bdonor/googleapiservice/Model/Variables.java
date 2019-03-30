a2afe26f5d08d53d2dd18aad77fbc41c2230d470
SweetTreats <sumite3117@hotmail.com>
2019-03-30 15:02:10 +0000

    remove api key

--- google-api-service/src/main/java/com/bdonor/googleapiservice/Model/Variables.java
+++ google-api-service/src/main/java/com/bdonor/googleapiservice/Model/Variables.java
@@ -3,7 +3,7 @@ package com.bdonor.googleapiservice.Model;
 public enum Variables {
 
 
-    API_KEY("AIzaSyBz-en4IzG0aeAxcGWc3Xo0fURt-Fb2-sU"),
+    API_KEY(""),
     PLACES_KEY(""),
     HIGH_RES("900x600"),
     MEDIUM_RES("600x480"),
