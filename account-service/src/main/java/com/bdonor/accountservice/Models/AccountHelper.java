57e8fd893a5ba714b29a81e5a75e9a8a34dafc6b
Sharjeel Jan <sharjeel50@hotmail.co.uk>
2019-04-22 20:11:42 +0100

    draft DynamoDB

--- account-service/src/main/java/com/bdonor/accountservice/Models/AccountHelper.java
+++ account-service/src/main/java/com/bdonor/accountservice/Models/AccountHelper.java
@@ -1,9 +1,10 @@
 package com.bdonor.accountservice.Models;
 
-import com.bdonor.accountservice.Repository.UserRepository;
-import com.bdonor.accountservice.Service.passwordEncoder;
+import com.bdonor.accountservice.Repository.DynamoDBRepo;
+//import com.bdonor.accountservice.Repository.UserRepository;
+//import com.bdonor.accountservice.Service.passwordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,110 +17,120 @@ import java.util.List;
 @Service
 public class AccountHelper {
 
-    @Autowired
-    private UserRepository UserRepo;
-
 //    @Autowired
-//    private BCryptPasswordEncoder bCryptPasswordEncoder;
+//    private UserRepository UserRepo;
+
+    private DynamoDBRepo dynamoDBRepo;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public User create(String bloodGroup, String firstname, String surname, String email, String password, String addressline, String postcode) { // This Works
-        User user = UserRepo.findByEmail(email);
-        if (user != null) {
-            System.out.println("User Exists");
-            return new User();
-        }
+//        User user = dynamoDBRepo.findByEmail(email);
+////        User user = UserRepo.findByEmail(email);
+//        if (user != null) {
+//            System.out.println("User Exists");
+//            return new User();
+//        }
 
-        String URLink;
-        String[] LatLong = {};
+//        String URLink;
+//        String[] LatLong = {};
+//
+//        try {
+//
+//            URL url = new URL(String.format("http://localhost:8110/geocoding/%s/%s", addressline, postcode));
+//            //System.out.println(url.toString());
+//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//            conn.setRequestMethod("GET");
+//            conn.setRequestProperty("Accept", "application/String");
+//
+//            if (conn.getResponseCode() != 200) {
+//                throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+//            }
+//
+//            String output = "E";
+//            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+//
+//            System.out.println("Output from Server .... \n");
+//            while ((output = br.readLine()) != null) {
+//                URLink = output;
+//                System.out.println(URLink);
+//                LatLong = URLink.split(",");
+//            }
+//
+//            conn.disconnect();
+//
+//        } catch (MalformedURLException e) {
+//
+//            e.printStackTrace();
+//
+//        } catch (IOException e) {
+//
+//            e.printStackTrace();
+//        }
 
-        try {
+        return dynamoDBRepo.save(new User(bloodGroup, firstname, surname, email, bCryptPasswordEncoder.encode(password), addressline, postcode));
 
-            URL url = new URL(String.format("http://localhost:8110/geocoding/%s/%s", addressline, postcode));
-            //System.out.println(url.toString());
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/String");
+        //return dynamoDBRepo.save(new User(bloodGroup, firstname, surname, email, bCryptPasswordEncoder.encode(password), addressline, postcode, LatLong[0], LatLong[1]));
 
-            if (conn.getResponseCode() != 200) {
-                throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
-            }
+       // return UserRepo.save(new User(bloodGroup, firstname, surname, email, bCryptPasswordEncoder.encode(password), addressline, postcode, LatLong[0], LatLong[1]));
 
-            String output = "E";
-            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-
-            System.out.println("Output from Server .... \n");
-            while ((output = br.readLine()) != null) {
-                URLink = output;
-                System.out.println(URLink);
-                LatLong = URLink.split(",");
-            }
-
-            conn.disconnect();
-
-        } catch (MalformedURLException e) {
-
-            e.printStackTrace();
-
-        } catch (IOException e) {
-
-            e.printStackTrace();
-        }
-
-        try {
-            passwordEncoder Encoder = new passwordEncoder();
-            return UserRepo.save(new User(bloodGroup, firstname, surname, email, Encoder.encrypt(password), addressline, postcode, LatLong[0], LatLong[1]));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return null;
     }
 
 
     public User getByfirstName(String firstname) {
-        return UserRepo.findByFirstName(firstname);
+
+        return dynamoDBRepo.findByFirstName(firstname);
+
+        //return UserRepo.findByFirstName(firstname);
     }
 
-    public List<User> getAll() { return UserRepo.findAll(); }
+//    public List<User> getAll() {
+//        return dynamoDBRepo.findAll();
+//        //return UserRepo.findAll();
+//    }
 
     public User Update(String bloodGroup, String firstname, String surname, String email, String password, String addressline, String postcode) {
 
-        try {
+        //User SpecificUser = UserRepo.findByFirstName(firstname);
+        User SpecificUser = dynamoDBRepo.findByFirstName(firstname);
+        SpecificUser.setBloodGroup(bloodGroup);
+        SpecificUser.setFirstname(firstname);
+        SpecificUser.setSurname(surname);
+        SpecificUser.setEmail(email);
+        SpecificUser.setPassword(bCryptPasswordEncoder.encode(password));
+        SpecificUser.setAddressline(addressline);
+        SpecificUser.setPostcode(postcode);
 
-            passwordEncoder aa = new passwordEncoder();
-            User SpecificUser = UserRepo.findByFirstName(firstname);
-            SpecificUser.setBloodGroup(bloodGroup);
-            SpecificUser.setFirstname(firstname);
-            SpecificUser.setSurname(surname);
-            SpecificUser.setEmail(email);
-            SpecificUser.setPassword(aa.encrypt(password));
-            SpecificUser.setAddressline(addressline);
-            SpecificUser.setPostcode(postcode);
+        return dynamoDBRepo.save(SpecificUser);
 
-            return UserRepo.save(SpecificUser);
+        //return UserRepo.save(SpecificUser);
 
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 
     public void deleteAll() {
-        UserRepo.deleteAll();
+
+        dynamoDBRepo.deleteAll();
+
+        //UserRepo.deleteAll();
     }
 
     public void deleteByfirstName(String firstname) {
-        User user = UserRepo.findByFirstName(firstname);
+        //User user = UserRepo.findByFirstName(firstname);
+        User user = dynamoDBRepo.findByFirstName(firstname);
         System.out.println(user);
-        UserRepo.delete(user);
+
+        dynamoDBRepo.delete(user);
+
+        //UserRepo.delete(user);
     }
 
     public boolean checkCredentials(String email, String password) {
         try {
-            User user = UserRepo.findByEmail(email);
-            passwordEncoder Decoder = new passwordEncoder();
+            User user = dynamoDBRepo.findByEmail(email);
+            //User user = UserRepo.findByEmail(email);
             if (user != null) {
-                if (Decoder.decrypt(password).equals(user.getPassword())) {
+                if (bCryptPasswordEncoder.matches(password,user.getPassword())) {
                     return true;
                 }
             }
