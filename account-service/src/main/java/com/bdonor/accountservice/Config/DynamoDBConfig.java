9290377256f83ea0179edc8d07fe955a958901b3
Sharjeel Jan <sharjeel50@hotmail.co.uk>
2019-04-29 21:10:48 +0100

    mapper bug fixed

--- account-service/src/main/java/com/bdonor/accountservice/Config/DynamoDBConfig.java
+++ account-service/src/main/java/com/bdonor/accountservice/Config/DynamoDBConfig.java
@@ -1,15 +1,15 @@
 package com.bdonor.accountservice.Config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-//import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
-
 
 @Configuration
 public class DynamoDBConfig {
