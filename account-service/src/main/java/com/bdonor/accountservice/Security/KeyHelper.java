8765f1dfca7d08cae5555c025cae81fda26d5f34
SweetTreats <sumite3117@hotmail.com>
2019-07-21 15:08:35 +0100

    update aws elastic ip

--- account-service/src/main/java/com/bdonor/accountservice/Security/KeyHelper.java
+++ account-service/src/main/java/com/bdonor/accountservice/Security/KeyHelper.java
@@ -25,7 +25,7 @@ public class KeyHelper {
         try {
 
 
-            URL url = new URL("http://52.56.76.0:5444/get-key/google/geocoding"); // special permissions set
+            URL url = new URL("http://18.130.137.35:5333/get-key/google/geocoding"); // special permissions set
 
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
