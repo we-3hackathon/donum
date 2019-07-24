0585642a5dbde1c85eaa7075682f083fe3d0a732
SweetTreats <sumite3117@hotmail.com>
2019-07-24 09:25:49 +0100

    Add body to sample tests
    Sample tests not been tested. How does the update user request works?

--- account-service/src/test/java/com/bdonor/accountservice/Controller/AccountTest.java
+++ account-service/src/test/java/com/bdonor/accountservice/Controller/AccountTest.java
@@ -29,30 +29,48 @@ public class AccountTest {
 
     @Test
     public void loadDynamoKeys() throws Exception {
+        this.mockMvc.perform(get("/api-key/load/dynamo-access")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("A")));
+        this.mockMvc.perform(get("/api-key/load/dynamo-secret")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("a")));
+
     }
 
     @Test
     public void testRegisterUser() throws Exception {
+        this.mockMvc.perform(get("/create/O/Ammy/Williams/williams2@gmail.com/amy9!##'as/29Bamfordavenue/ha01na")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("User added")));
     }
 
     @Test
     public void testUserCanLogin() throws Exception {
+        this.mockMvc.perform(get("/login/ammy/williams2@gmail.com/amy9!##'as")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Successful")));
     }
 
     @Test
     public void testRetrieveUsersInDB() throws Exception {
+        this.mockMvc.perform(get("/get-all")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("firstname")));
     }
 
     @Test
     public void testDeleteUser() throws Exception {
+        this.mockMvc.perform(get("/delete/ammy/williams")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Deleted")));
+        this.mockMvc.perform(get("/getUser/ammy/williams")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("not found")));
     }
 
     @Test
     public void testUpdateUser() throws Exception {
+        //
     }
 
     @Test
     public void testGetSpecificUser() throws Exception {
+        this.mockMvc.perform(get("/getUser/ammy/williams")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("ammy")));
     }
 
 }
