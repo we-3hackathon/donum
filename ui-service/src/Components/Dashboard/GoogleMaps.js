dea482232e9adebc48f6a5e6b372263f2b0ed7f5
Sharjeel50 <sharjeel50@hotmail.co.uk>
2019-08-14 01:38:53 +0100

    State updated with Users, needs testing.

--- ui-service/src/Components/Dashboard/GoogleMaps.js
+++ ui-service/src/Components/Dashboard/GoogleMaps.js
@@ -3,50 +3,42 @@ import React, { Component } from "react";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
 export class GoogleMaps extends Component {
-    state = {
-      usersLocation: null
-    };
+  state = {
+    UserInfo: null
+  };
 
-    // *** Add componentDidMount to start the async load
-    componentDidMount() {
-      this.getUserLocation()
-      .then(location => {
-        this.setState({
-            usersLocation: location.map(user => {key: user.id, lat: user.latitude, lng: user.longitude})
-      }).catch(error => {
+  componentDidMount() {
+    var self = this;
+    axios
+      .get(`http://40.121.148.131:8000/account-service/get-all`)
+      .then(response => {
+        self.setState({ UserInfo: response.data }, () => {
+          console.log("state updated to", this.state);
+        });
+      })
+      .catch(function(error) {
         console.log(error);
       });
-      console.log(this.state);
-    })
   }
 
-
-    render() {
-      const { location } = this.state;
-      console.log(location);
-      if (!location) {
-        return <div>Loading Map...</div>;
-      }
-      return (
-        <Map google={this.props.google} zoom={14}>
-          {location.map(User => (
-            <Marker
-              key={User.id}
-              position={{ lat: User.latitude, lng: User.longitude }}
-            />
-          ))}
-        </Map>
-      );
+  render() {
+    const { UserInfo } = this.state;
+    if (!UserInfo) {
+      return <div>Loading Map...</div>;
     }
-
-    getUserLocation() {
-      // *** *Return* the result of the axios call
-      return axios.get("http://40.121.148.131:8000/account-service/get-all").then(UserDetails => {
-        return UserDetails.data
-      });
-    }
+    return (
+      <Map google={this.props.google} zoom={5}>
+        {this.state.UserInfo.map(User => (
+          <Marker
+            key={User.id}
+            position={{ lat: User.latitude, lng: User.longitude }}
+          />
+        ))}
+      </Map>
+    );
   }
+}
 
 export default GoogleApiWrapper({
-  apiKey: "KEY"
+  apiKey: "key"
 })(GoogleMaps);
