115b7f6a3ac568af27f626e0e3834526e5cecd6a
Sharjeel50 <sharjeel50@hotmail.co.uk>
2019-09-21 14:02:02 +0100

    JWT token service

---
+++ jwt-service/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, escape, request, json, jsonify
+import datetime
+import jwt
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'YourContribution'
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = "need to do"
+
+        if not token:
+            return "whatever"
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+        except:
+            return "need to do returns"
+
+        return f(*args, **kwargs)
+    
+    return decorated
+
+@app.route('/token-creation/user', methods=['GET', 'POST'])
+def create_JWT():
+    try:
+        user = request.json
+        token = jwt.encode({'email': user["user"], 'password': user["password"],'exp': datetime.datetime.utcnow()
+                               + datetime.timedelta(minutes=120)}, app.config["SECRET_KEY"])
+        token = jsonify({'token': token.decode('UTF-8')})
+        return token, 201
+    except Exception as e:
+        return e, 400
+        
+
+if __name__ == "__main__":
+    app.run(debug=True)
