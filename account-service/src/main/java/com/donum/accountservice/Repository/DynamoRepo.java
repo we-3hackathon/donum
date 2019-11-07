a89d481d62d2cac6d69163dfa76a4ab624f6f971
SweetTreats <sumite3117@hotmail.com>
2019-11-07 15:07:48 +0000

    Enhancements (enum and
    * new enum for email fields
    * defensive coding against coordinates

--- account-service/src/main/java/com/donum/accountservice/Repository/DynamoRepo.java
+++ account-service/src/main/java/com/donum/accountservice/Repository/DynamoRepo.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 import com.donum.accountservice.Controller.APIKeyController;
+import com.donum.accountservice.Enum.EmailFields;
 import com.donum.accountservice.Enum.Template_Paths;
 import com.donum.accountservice.InternalService.JWTService;
 import com.donum.accountservice.InternalService.GoogleApiServiceHelper;
@@ -21,6 +22,8 @@ import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.ComparisonOperator;
 import com.amazonaws.services.dynamodbv2.model.ConditionalCheckFailedException;
 import com.amazonaws.services.dynamodbv2.model.ExpectedAttributeValue;
+
+import javax.validation.constraints.Email;
 
 public class DynamoRepo {
 
@@ -41,24 +44,41 @@ public class DynamoRepo {
             return 1;
         }
 
-        String[] LatLong = GoogleApiServiceHelper.convertToCoordinates(user.getAddressline(), user.getPostcode());
-
-        if (LatLong[0].contains("Not Found") || LatLong[1].contains("Not Found")){ return 2; }
-
         String randID = UUID.randomUUID().toString();
-
         user.setId(randID);
-        user.setLatitude(LatLong[0]);
-        user.setLongitude(LatLong[1]);
 
-        APIKeyController._singleDynamoMapper.save(user);
+        try {
+            String[] LatLong = GoogleApiServiceHelper.convertToCoordinates(user.getAddressline(), user.getPostcode());
+
+            // location could not be determined
+            if(LatLong[0].isEmpty() || LatLong[1].isEmpty()){
+                user.setLatitude("500");
+                user.setLongitude("500");
+
+                APIKeyController._singleDynamoMapper.save(user);
+            }
+
+            user.setLatitude(LatLong[0]);
+            user.setLongitude(LatLong[1]);
+
+            APIKeyController._singleDynamoMapper.save(user);
+
+        }catch (Exception e){
+
+            // 500 error recorded, user can still register
+            user.setLatitude("500");
+            user.setLongitude("500");
+
+            APIKeyController._singleDynamoMapper.save(user);
+        }
+
 
         try {
             Map<String, Object> emailData = new HashMap<>();
             emailData.put("Name", user.getFirstName() + " " + user.getLastName());
 
-            emailService.sendEmail(new MailRequest(randID, user.getFirstName(), user.getLastName(), "Aroundhackathon@gmail.com",
-                            "Email Confirmation"), emailData, Template_Paths.EMAIL_CONFIRMATION.toString());
+            emailService.sendEmail(new MailRequest(randID, user.getFirstName(), user.getLastName(), EmailFields.FROM.toString(),
+                            EmailFields.CONFIRMATION_SUBJECT.toString()), emailData, Template_Paths.EMAIL_CONFIRMATION.toString());
         }catch(Exception e){
             e.printStackTrace();
         }
@@ -93,8 +113,8 @@ public class DynamoRepo {
                 updateUserDetail(email, 5, ResetToken);
             }
             emailData.put("ResetURL", "http://localhost:3000/resetpassword" + ResetToken + "/" + email);
-            emailService.sendEmail(new MailRequest(email, "Aroundhackathon@gmail.com",
-                    "Reset Password"), emailData, Template_Paths.RESET_PASSWORD.toString());
+            emailService.sendEmail(new MailRequest(email, EmailFields.FROM.toString(),
+                    EmailFields.RESET_SUBJECT.toString()), emailData, Template_Paths.RESET_PASSWORD.toString());
             return 1;
         }catch (Exception e){
             e.printStackTrace();
